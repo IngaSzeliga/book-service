d7f48251115f1ec6961efeea27e21367c7d96948
Inga Szeliga <inga.szeliga@gmail.com>
2019-10-01 19:05:22 +0200

    Added select option in Search component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
+import MenuItem from "@material-ui/core/MenuItem";
 import "./SearchBar.scss";
 
-const SearchBar = () => {
-  return (
-    <div className="search-bar-component">
-      <form>
-        <TextField
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            )
-          }}
-          label="Search"
-          margin="normal"
-          variant="outlined"
-        />
-      </form>
-    </div>
-  );
-};
+class SearchBar extends PureComponent {
+  state = {
+    selectSearch: "authors"
+  };
 
-export default React.memo(SearchBar);
+  handleChange = event => {
+    this.setState({ selectSearch: event.target.value });
+  };
+
+  render() {
+    const { selectSearch } = this.state;
+    return (
+      <div className="search-select-container">
+        <form>
+          <TextField
+            className="search-select-component"
+            select
+            defaultValue="Authors"
+            value={selectSearch}
+            onChange={this.handleChange}
+            margin="normal"
+            variant="outlined"
+          >
+            <MenuItem value="authors">Authors</MenuItem>
+            <MenuItem value="books">Books</MenuItem>
+          </TextField>
+          <TextField
+            className="search-bar-component"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+            label="Search"
+            margin="normal"
+            variant="outlined"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
